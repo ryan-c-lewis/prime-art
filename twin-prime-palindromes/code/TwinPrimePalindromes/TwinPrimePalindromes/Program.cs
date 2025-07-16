4dae404b057c36ec41552a556670137c25a593ac
ryan-c-lewis <ryan.cp.lewis@protonmail.com>
2025-07-16 01:48:02 -0500

    clean up so others can understand what's going on

--- twin-prime-palindromes/code/TwinPrimePalindromes/TwinPrimePalindromes/Program.cs
+++ twin-prime-palindromes/code/TwinPrimePalindromes/TwinPrimePalindromes/Program.cs
@@ -28,18 +28,24 @@ public class Program
         Dictionary<BigInteger, string> binaryPalindromePrimeMiddles = new Dictionary<BigInteger, string>();
         Dictionary<int, double> proportionThatAreTwinPrimeMiddles = new Dictionary<int, double>();
 
-        int minLength = 1;
-        int maxLength = 40;
+        int minLength = 1; // start at binary numbers with this many digits
+        int maxLength = 40; // end at binary numbers with this many digits
         for (int length = minLength; length < maxLength; length++)
         {
-            List<string> base10Palindromes = PalindromeGenerator.GenerateBinaryPalindromes(length);
+            // Generate all palindromes of this length. e.g. for length 4 it would be:
+            //  0000
+            //  0110
+            //  1001
+            //  1111
+            List<string> binaryPalindromes = PalindromeGenerator.GenerateBinaryPalindromes(length);
             int twinPrimeMiddleCount = 0;
             
-            foreach (string binaryPalindrome in base10Palindromes)
+            foreach (string binaryPalindrome in binaryPalindromes)
             {
                 try
                 {
                     BigInteger binaryPalindromeValue = BinaryStringToBigInteger(binaryPalindrome);
+                    // Perfectly precise prime checking would be much slower and probably not change the result very much
                     bool isTwinPrimeMiddle = PrimeChecker.IsProbablyPrime(binaryPalindromeValue - 1)
                                              && PrimeChecker.IsProbablyPrime(binaryPalindromeValue + 1);
 
@@ -56,7 +62,8 @@ public class Program
                 }
             }
 
-            proportionThatAreTwinPrimeMiddles[length] = (double)twinPrimeMiddleCount / base10Palindromes.Count;
+            // TODO I assume this is changing similarly to what would be expected from the prime number theorem, but I haven't checked
+            proportionThatAreTwinPrimeMiddles[length] = (double)twinPrimeMiddleCount / binaryPalindromes.Count;
             Console.WriteLine($"Proportion at length {length}: {proportionThatAreTwinPrimeMiddles[length]}");
             File.AppendAllLines(fileNameForProportions, [$"{length},{proportionThatAreTwinPrimeMiddles[length]}]"]);
         }
@@ -73,6 +80,7 @@ public class Program
         }
 
         int longestBinaryLength = binaryPalindromePrimeMiddles.Values.Max(x => x.Length);
+        // Decision about how to order the results is important... what are we actually trying to accomplish? It's not obvious.
         List<KeyValuePair<BigInteger, string>> orderedPalindromes = binaryPalindromePrimeMiddles
             .OrderBy(pair => TrimForOrdering(pair.Value).Length)
             .ThenBy(x => x.Key)
@@ -80,8 +88,10 @@ public class Program
             
         foreach (KeyValuePair<BigInteger, string> palindrome in orderedPalindromes)
         {
-            File.AppendAllLines(fileNameForRawPrimes, [palindrome.Value]);
+            // Dump the raw values into a file so we can have that if needed
+            File.AppendAllLines(fileNameForRawPrimes, [palindrome.Key + "," + palindrome.Value]);
             
+            // Create an ASCII visual equivalent to the bitmap one
             StringBuilder line =  new StringBuilder();
             int paddingToAddPerSide = (longestBinaryLength - palindrome.Value.Length);
             for (int n = 0; n < paddingToAddPerSide; n++)
@@ -89,11 +99,11 @@ public class Program
             line.Append(palindrome.Value.Replace("0", "  ").Replace("1", "██"));
             for (int n = 0; n < paddingToAddPerSide; n++)
                 line.Append(' ');
-            
             Console.WriteLine(line);
             File.AppendAllLines(fileNameForRawVisual, [line.ToString()]);
         }
         
+        // Create a bitmap visual. This file isn't formatted properly to be opened in most apps but a browser can open it.
         RawBitmap bmp = new(longestBinaryLength * 2, orderedPalindromes.Count * 2);
         for (int y = 0; y < orderedPalindromes.Count; y++)
         {
